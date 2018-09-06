130d8fcf684200896600527978bcb30a9f27d63f
Ortega, Juan <juan.ortega@warnerbros.com>
2018-09-05 22:05:27 -0700

    Bug Fix: Added code to continue loop when program crashes

--- trafficBeat.py
+++ trafficBeat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
 # NAME - trafficBeat.py
-# Version 1.2
+# Version 1.3
 #
 # SYNOPSIS
 # python3 ./trafficBeat.py 10.0.0.1
@@ -18,13 +18,18 @@
 # 2018-09-02            1.0             Juan Ortega     First Working Version of Script
 # 2018-09-02            1.1             Juan Ortega     Windows x86-64 Binary Support
 # 2018-09-03            1.2             Juan Ortega     Bug Fix: Fixed Endless Loop of Duplicate Packet Bug
+# 2018-09-04            1.3             Juan Ortega     Bug Fix: Added code to continue loop when program crashes
 
 import sys
 from scapy.all import *
 
 
 def main():
-    sniff(prn=chg_mac, filter='ip', store=0)
+    while 1:
+        try:
+            sniff(prn=chg_mac, filter='ip', store=0)
+        except:
+            continue
 
 
 def get_mac(monip):
@@ -68,7 +73,7 @@ if __name__ == "__main__":
     try:
         monip = sys.argv[1]
     except:
-        print("===============Error=============== \n"
+        print("===============trafficBeat=============== \n"
               "Pass the IP you wish to mirror traffic to.\n"
               "This application will edit every packet and modify the DST MAC\n"
               "with the MAC address associated with the IP entered. Modified packet\n"
